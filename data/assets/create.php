76f003f521c5540fbc45884f14a2b7c451686adc
BonyChops <office.kentarou@gmail.com>
2020-08-05 23:05:28 +0900

    Edit

--- data/assets/create.php
+++ data/assets/create.php
@@ -12,13 +12,13 @@
         <div class="row">
             <h5>タイトル</h5>
             <div class="input-field col s12">
-                <input id="title"  pattern=".*\S+.*" type="text">
+                <input id="title"  pattern=".*\S+.*" required type="text">
                 <label for="title">わかりやすくて簡潔なタイトル...</label>
                 <span class="helper-text" data-error="空白で投稿はできません" data-success="">Helper text</span>
             </div>
             <h5>内容</h5>
             <div class="input-field col s12">
-                <textarea id="textarea1"  pattern=".*\S+.*" class="materialize-textarea"></textarea>
+                <textarea id="textarea1" required pattern=".*\S+.*" class="materialize-textarea"></textarea>
                 <label for="textarea1">内容(改行OK)...</label>
                 <span class="helper-text" data-error="空白で投稿はできません" data-success="">Helper text</span>
             </div>
