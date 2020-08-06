c5061fd9f9d336808d1ef1c46d4bc7b3a8c1eecb
BonyChops <office.kentarou@gmail.com>
2020-08-06 16:55:48 +0900

    Edit

--- oauth/google/callback.php
+++ oauth/google/callback.php
@@ -64,7 +64,7 @@ session_start();
 $_SESSION['GOOGLE_ACCESS_TOKEN'] = $accessToken;
 $_SESSION['GOOGLE_USER_INFO'] = $userInfo;
 
-if(($oauthKey = findThread($userInfo['id'], $oauthTokens, true)) === false){
+if(($oauthKey = findThread($userInfo->{'id'}, $oauthTokens, true)) === false){
         array_push($data['oauthTokens'], $userInfo);
 }else{
         $data['oauthTokens'][$oauthKey] = $userInfo;
