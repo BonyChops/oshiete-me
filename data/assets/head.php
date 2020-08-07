3a514480753359a7e957d9f6fe391394ee609bbf
BonyChops <office.kentarou@gmail.com>
2020-08-07 17:41:25 +0900

    Edit

--- data/assets/head.php
+++ data/assets/head.php
@@ -10,7 +10,7 @@ if(!$loggedIn){
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-    <title>おしえてME for 3J</title>
+    <title>おしえてME</title>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
