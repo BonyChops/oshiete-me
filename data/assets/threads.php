4c308162f3e910f17b4f137b0f000db8a6c7307b
BonyChops <office.kentarou@gmail.com>
2020-08-16 20:42:12 +0900

    Edit

--- data/assets/threads.php
+++ data/assets/threads.php
@@ -5,7 +5,7 @@ function linkDetect($text)
 {
     $pattern = '/((?:https?|ftp):\/\/[-_.!~*\'()a-zA-Z0-9;\/?:@&=+$,%#]+)/';
     $replace = '<a href="$1">$1</a>';
-    $text    = preg_replace($pattern, $replace, $text);
+    return preg_replace($pattern, $replace, $text);
 }
 
 
