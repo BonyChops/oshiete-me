d789517258c2b4f1c6282550adc8f5f27df333ee
BonyChops <office.kentarou@gmail.com>
2020-08-20 00:12:18 +0900

    Edit

--- data/assets/head.php
+++ data/assets/head.php
@@ -1,10 +1,11 @@
 <?php
 //以下有効で常時ログイン必須に
-if(!$loggedIn){
-    header('location: '.googleLoginURI());
+if (!$loggedIn) {
+    header('location: ' . googleLoginURI());
     exit();
 }
 ?>
+
 <head>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -13,4 +14,20 @@ if(!$loggedIn){
     <title>おしえてME</title>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Enable TeX -->
+    <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+    </script>
+    <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({
+        TeX: { equationNumbers: { autoNumber: "AMS" }},
+        tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+        processEscapes: true
+        },
+        "HTML-CSS": { matchFontHeight: false },
+        displayAlign: "left",
+        displayIndent: "2em"
+    });
+    </script>
+    <!-- Enable TeX -->
 </head>
